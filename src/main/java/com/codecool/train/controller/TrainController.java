fe15e631e6bd70d34f3205f168f3e7a31f00672c
zoli73 <zolee73.dev@gmail.com>
2023-04-12 09:56:39 +0200

    implement TrainController

---
+++ src/main/java/com/codecool/train/controller/TrainController.java
@@ -0,0 +1,42 @@
+package com.codecool.train.controller;
+
+import com.codecool.train.dto.TrainDto;
+import com.codecool.train.dto.TrainWagonDto;
+import com.codecool.train.entity.Train;
+import com.codecool.train.service.TrainService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/train")
+public class TrainController {
+    private TrainService trainService;
+
+    public TrainController(TrainService trainService) {
+        this.trainService = trainService;
+    }
+
+    @PostMapping
+    public void saveTrain(@RequestBody TrainDto trainDto) {
+        trainService.saveTrain(trainDto);
+    }
+
+    @PostMapping
+    public void addWagonToTrain(@RequestBody TrainWagonDto trainWagonDto) {
+        trainService.addWagonToTrain(trainWagonDto);
+    }
+
+    @GetMapping("/{id}")
+    public Train getTrainById(@PathVariable("id") String id) {
+        return trainService.getTrainById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateTrain(@PathVariable("id") String id, @RequestBody TrainDto trainDto) {
+        trainService.updateTrain(id, trainDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTrain(@PathVariable("id") String id) {
+        trainService.deleteTrain(id);
+    }
+}
