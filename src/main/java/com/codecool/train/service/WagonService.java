c1070a47e77f0dfd100265f24e5afc737ff6cf1d
zoli73 <zolee73.dev@gmail.com>
2023-04-12 10:35:39 +0200

    implement WagonService

--- src/main/java/com/codecool/train/service/WagonService.java
+++ src/main/java/com/codecool/train/service/WagonService.java
@@ -1,7 +1,14 @@
 package com.codecool.train.service;
 
+import com.codecool.train.dto.WagonDto;
+import com.codecool.train.dto.mapper.WagonMapper;
+import com.codecool.train.entity.Wagon;
 import com.codecool.train.repository.WagonDAO;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class WagonService {
@@ -11,5 +18,30 @@ public class WagonService {
         this.wagonDAO = wagonDAO;
     }
 
+    public void saveWagon(WagonDto wagonDto) {
+        wagonDAO.save(WagonMapper.dtoToWagon(wagonDto));
+    }
 
+    public List<Wagon> listAllWagon() {
+        return wagonDAO.findAll();
+    }
+
+    public Wagon getWagonById(String id) {
+        return wagonDAO.findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.BAD_REQUEST, "Wagon not found with the given id!"));
+    }
+
+    public void updateWagon(String id, WagonDto wagonDto) {
+        Wagon wagon = getWagonById(id);
+        wagon.setId(wagonDto.getId());
+        wagon.setWeight(wagonDto.getWeight());
+        wagon.setWagonType(wagonDto.getWagonType());
+        wagonDAO.save(wagon);
+    }
+
+    public void deleteWagon(String id) {
+        Wagon wagon = getWagonById(id);
+        wagonDAO.delete(wagon);
+    }
 }
